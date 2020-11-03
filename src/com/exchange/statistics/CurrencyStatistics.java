713fdd9a9800f5b037b61974c5a81248909b135e
gabrielbutnariu <butnariu.gabriel24@gmail.com>
2020-11-04 01:49:53 +0200

    currency count up/down and mean value

---
+++ src/com/exchange/statistics/CurrencyStatistics.java
@@ -0,0 +1,27 @@
+package com.exchange.statistics;
+
+public class CurrencyStatistics {
+    private int cntCurrencyUp = 0, cntCurrencyDown = 0;
+    private double meanCurrencyValue = 0;
+    private String symbol = "";
+
+    public CurrencyStatistics(int cntCurrencyUp, int cntCurrencyDown, double meanCurrencyValue, String symbol){
+        this.cntCurrencyUp = cntCurrencyUp;
+        this.cntCurrencyDown = cntCurrencyDown;
+        this.meanCurrencyValue = meanCurrencyValue;
+        this.symbol = symbol;
+    }
+
+    public int getCntCurrencyUp(){
+        return cntCurrencyUp;
+    }
+    public int getCntCurrencyDown(){
+        return cntCurrencyDown;
+    }
+    public double getMeanCurrencyValue(){
+        return meanCurrencyValue;
+    }
+    public String getSymbol(){
+        return symbol;
+    }
+}
